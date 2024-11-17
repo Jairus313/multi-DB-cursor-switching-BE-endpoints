dda2a4945194658a8038c044ddd4bf483ad47aac
Sudeep Nellur <sudeepnellur@Sudeeps-MacBook-Pro.local>
2024-11-17 19:22:32 +0530

    session maker

--- app/db/session.py
+++ app/db/session.py
@@ -4,12 +4,11 @@ from app.core.config import DBConfig
 
 
 engine = create_engine(DBConfig.database_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+MasterSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def get_user_db_session(user_db_url):
+    engine = create_engine(user_db_url)
+    session_local = sessionmaker(bind=engine)
+
+    return session_local
